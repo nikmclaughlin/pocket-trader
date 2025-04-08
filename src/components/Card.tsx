aef01ef119e9e784103fdc07638f30ae7a7a4cd9
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2025-04-07 23:04:47 -0700

    account tabs

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,29 +4,27 @@ export const Card = (params: { cardData: Doc<'cards'> }) => {
   const cardData = params.cardData
 
   return (
-    <div className="border-2 rounded p-2 max-w-80 flex flex-col items-center">
+    <div className="border-2 rounded p-2 max-w-80 flex flex-col items-center font-base text-foreground">
       <div className="flex w-full justify-between items-center">
-        <h2 className="text-2xl font-bold text-center">{cardData.name}</h2>
-        <p className="text-center text-sm text-stone-600">
-          Card ID: {cardData.cardNo}
-        </p>
+        <h2 className="text-2xl font-heading">{cardData.name}</h2>
+        <p className="text-center text-sm">Card ID: {cardData.cardNo}</p>
       </div>
       <img
         className="w-80 h-80"
         src={cardData.image.length ? cardData.image : null}
       />
-      <div className="flex w-full justify-between">
+      <div className="flex w-full justify-between text-sm">
         <p>Illus: {cardData.artist}</p>
         <p>Set: {cardData.set}</p>
       </div>
       {/* <div className="border rounded p-2 bg-stone-200">
         <p className="text-xl ">{cardData.description}</p>
       </div> */}
-      <div className="border rounded p-2 bg-stone-200 h-32">
+      <div className="border rounded p-2 bg-secondary-background h-32">
         <p className="italic">{cardData.flavor}</p>
       </div>
       <div className="flex w-full justify-end">
-        <p>Rarity: {cardData.rarity}</p>
+        <p className="text-sm">Rarity: {cardData.rarity}</p>
       </div>
     </div>
   )
--- src/pages/CurrentAccount.tsx
+++ src/pages/CurrentAccount.tsx
@@ -5,6 +5,11 @@ import { AddCardModal } from '../components/AddCardModal'
 import { Card } from '../components/Card'
 
 import { Button } from '@/components/ui/button'
+
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
 import { useState } from 'react'
 import { FriendIdForm } from '../components/FriendIdForm'
 
@@ -20,45 +25,55 @@ export const CurrentAccount = () => {
   return (
     <>
       <div className="flex flex-col w-full max-w-6xl">
-        {/* <FriendIdForm /> */}
-        <div className="self-end flex flex-col items-end p-4 border-4 border-double rounded transition-all">
-          <p className="text-xl text-right">{`WELCOME ${currentUser?.name?.toUpperCase() || 'BACK!'}`}</p>
-          <p className="text-md text-right text-stone-700">{`FRIEND ID: ${currentUser?.friendId || '---'}`}</p>
-          {/* TODO: Edit Profile */}
-          {isEditingProfile && <FriendIdForm />}
-          <div className="flex gap-2 p-2">
-            {isEditingProfile ? (
-              <Button onClick={() => setIsEditingProfile(false)}>
-                Save Changes
-              </Button>
-            ) : (
-              <Button
-                variant="reverse"
-                onClick={() => setIsEditingProfile(true)}
-              >
-                Edit Profile
-              </Button>
-            )}
+        <Tabs defaultValue="account">
+          <TabsList className="grid w-full grid-cols-3">
+            <TabsTrigger value="account">Account</TabsTrigger>
+            <TabsTrigger value="wishlist">Wishlist</TabsTrigger>
+            <TabsTrigger value="catalog">Catalog</TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">
+            <div className=" flex flex-col p-4 ">
+              <p className="text-xl font-heading">{`WELCOME ${currentUser?.name?.toUpperCase() || 'BACK!'}`}</p>
+              <p className="text-md font-base text-foreground">{`FRIEND ID: ${currentUser?.friendId || '---'}`}</p>
+              {/* TODO: Edit Profile */}
+              {isEditingProfile && <FriendIdForm />}
+              <div className="flex gap-2 p-2">
+                {isEditingProfile ? (
+                  <Button onClick={() => setIsEditingProfile(false)}>
+                    Save Changes
+                  </Button>
+                ) : (
+                  <Button
+                    variant="reverse"
+                    onClick={() => setIsEditingProfile(true)}
+                  >
+                    Edit Profile
+                  </Button>
+                )}
 
-            <Button variant="reverse" onClick={() => void signOut()}>
-              Sign Out
-            </Button>
-          </div>
-        </div>
-        <p className="font-heading text-2xl">MY WISHLIST</p>
-
-        {wishlistCards ? (
-          <>
-            <AddCardModal />
-            <div className="flex gap-2 w-full p-2">
-              {wishlistCards.map((card) => {
-                return <Card cardData={card} key={card._id} />
-              })}
+                <Button variant="reverse" onClick={() => void signOut()}>
+                  Sign Out
+                </Button>
+              </div>
             </div>
-          </>
-        ) : (
-          <Button onClick={() => createWishlist()}>Create Wishlist</Button>
-        )}
+          </TabsContent>
+          <TabsContent value="wishlist">
+            <p className="font-heading text-2xl">MY WISHLIST</p>
+
+            {wishlistCards ? (
+              <>
+                <AddCardModal />
+                <div className="flex gap-2 w-full p-2">
+                  {wishlistCards.map((card) => {
+                    return <Card cardData={card} key={card._id} />
+                  })}
+                </div>
+              </>
+            ) : (
+              <Button onClick={() => createWishlist()}>Create Wishlist</Button>
+            )}
+          </TabsContent>
+        </Tabs>
       </div>
     </>
   )
