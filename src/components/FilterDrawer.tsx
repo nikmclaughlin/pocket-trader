d80bc77bc5c1885002b6f341b680965b2ae3b0ba
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2025-05-08 10:57:06 -0700

    add multi-select filters

--- src/components/FilterDrawer.tsx
+++ src/components/FilterDrawer.tsx
@@ -8,29 +8,102 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from '@/components/ui/drawer'
-import { cardIdSets } from '@/lib/utils'
-import { useEffect, useState } from 'react'
+import {
+  cardIdSets,
+  cardRarities,
+  sanitizeFileName,
+  ValueOf,
+} from '@/lib/utils'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Card } from 'convex/schema'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form } from './ui/form'
 import { Multiselect } from './ui/multiselect'
 
-const setList = Object.values(cardIdSets)
+export type FilterRule = {
+  factor: keyof Card
+  values: string[]
+}
 
-const setOptions = setList.map((setName) => {
+const setOptions = Object.keys(cardIdSets).map((setId) => {
+  const setName = cardIdSets[setId as keyof typeof cardIdSets]
+  const setIcon = setName
+    ? `/set_logos/${sanitizeFileName(setName)}.png`
+    : undefined
   return {
     value: setName,
     label: setName,
+    icon: setIcon,
   }
 })
 
-export const FilterDrawer = () => {
+const rarityOptions = Object.keys(cardRarities).map((rarity) => {
+  const rarityIcon =
+    rarity === 'Promo'
+      ? undefined
+      : `/rarities/${sanitizeFileName(cardRarities[rarity as keyof typeof cardRarities])}.png`
+  return {
+    value: rarity,
+    label: rarity,
+    icon: rarityIcon,
+  }
+})
+
+const getSetIdByName = (setName: ValueOf<typeof cardIdSets>) => {
+  const keys = Object.keys(cardIdSets)
+  const setIdKey = keys.find(
+    (key) => cardIdSets[key as keyof typeof cardIdSets] === setName
+  )
+  return setIdKey
+}
+
+export const FilterDrawer = (props: {
+  submitCb: (filters: FilterRule[]) => void
+}) => {
+  const { submitCb } = props
   const [currentSetFilter, setCurrentSetFilter] = useState(setOptions)
+  const [currentRarityFilter, setCurrentRarityFilter] = useState(rarityOptions)
 
   const handleSetSelect = (selections: typeof setOptions) => {
     setCurrentSetFilter(selections)
   }
 
-  useEffect(() => {
-    // console.log(currentSetFilter)
-  }, [currentSetFilter])
+  const handleRaritySelect = (selections: typeof rarityOptions) => {
+    setCurrentRarityFilter(selections)
+  }
+
+  const multiselectOptionSchema = z.object({
+    value: z.string(),
+    label: z.string(),
+    icon: z.union([z.string(), z.undefined()]),
+  })
+
+  const formSchema = z
+    .object({
+      set: multiselectOptionSchema,
+      rarity: multiselectOptionSchema,
+    })
+    .partial()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  })
+
+  const handleSubmit = () => {
+    const setIds: string[] = []
+    currentSetFilter.map((set) => {
+      const setId = getSetIdByName(set.value)
+      if (setId) setIds.push(setId)
+    })
+    const rarityValues = currentRarityFilter.map((r) => r.value)
+
+    submitCb([
+      { factor: 'id', values: setIds },
+      { factor: 'rarity', values: rarityValues },
+    ])
+  }
 
   return (
     <Drawer direction="right">
@@ -41,12 +114,30 @@ export const FilterDrawer = () => {
         <DrawerHeader>
           <DrawerTitle>Adjust Filters</DrawerTitle>
         </DrawerHeader>
-        <div className="overflow-y-auto px-4 text-sm">
-          <h4 className="text-lg leading-none font-heading">Release Set</h4>
-          <Multiselect options={setOptions} selectCb={handleSetSelect} />
-        </div>
+        <Form {...form}>
+          <form className="flex flex-col gap-4">
+            <div className="overflow-y-auto px-4 text-sm flex flex-col gap-2">
+              <h4 className="text-lg leading-none font-heading">Release Set</h4>
+              <Multiselect
+                options={setOptions}
+                selected={currentSetFilter || []}
+                selectCb={handleSetSelect}
+              />
+            </div>
+            <div className="overflow-y-auto px-4 text-sm flex flex-col gap-2">
+              <h4 className="text-lg leading-none font-heading">Rarity</h4>
+              <Multiselect
+                options={rarityOptions}
+                selected={currentRarityFilter || []}
+                selectCb={handleRaritySelect}
+              />
+            </div>
+          </form>
+        </Form>
         <DrawerFooter>
-          <Button>Submit</Button>
+          <DrawerClose asChild>
+            <Button onClick={handleSubmit}>Submit</Button>
+          </DrawerClose>
           <DrawerClose asChild>
             <Button variant="neutral">Cancel</Button>
           </DrawerClose>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+export type ValueOf<T> = T[keyof T]
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
