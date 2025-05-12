fc2435c5cfa2f1644737cb7d7e2267fe03496cf1
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2025-05-11 17:29:04 -0700

    move full catalog off of home page
    reduce passive db usage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import { NavBar } from './components/NavBar'
 import { ThemeProvider } from './context/ThemeProvider'
 import { Account } from './pages/Account'
+import { FullCatalog } from './pages/FullCatalog'
 import { Home } from './pages/Home'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route element={<NavBar />}>
             <Route path="/" element={<Home />} />
             <Route path="/account/:accountId" element={<Account />} />
+            <Route path="/catalog" element={<FullCatalog />} />
           </Route>
         </Routes>
       </ThemeProvider>
---
+++ src/pages/FullCatalog.tsx
@@ -0,0 +1,5 @@
+import { CardCatalog } from '@/components/CardCatalog'
+
+export const FullCatalog = () => {
+  return <CardCatalog className="w-dvw max-w-7xl px-4" />
+}
